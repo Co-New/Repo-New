cbe5c07d32fa9b20daa36f145dc5d8e6e556b005
Mustare <maxstar2995@gmail.com>
2015-08-27 10:43:12 +0530

    Math.cpp
    I know only this much!

---
+++ Math.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namesapce std;
+
+int main()
+{
+
+  int a=20, b=2, c;
+  
+  cout<<"The addition is: "<<a+b;
+  
+  cout<<"The subtraction is: "<<a-b;
+  
+  cout<<"The multiplication is: "<<a*b;
+  
+  cout<<"The division is: "<<a/b;
+  
+  cout<<"The square root of 20 is: "<<sqrt(a);
+  
+  return 0;
+}
